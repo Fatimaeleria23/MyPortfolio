05cd0dda408ea7e0f711ecef266ed57a8c4ba620
Kriszle Fatima Eleria <eleriakriszle@gmail.com>
2024-10-19 10:14:27 +0800

    Delete Simone-Template-main directory

--- Simone-Template-main/js/animation.js
+++
@@ -1,8 +0,0 @@
-import Typed from "./typed.js";
-
-const autoType = new Typed(".typed", {
-  strings: ["I'm a Developer", "I'm a Freelancer", "I'm a Designer"],
-  typeSpeed: 90,
-  backSpeed: 90,
-  loop: true,
-});
